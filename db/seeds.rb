26999d7b91162717efa3a2ca0702db269adbf1c0
Pedro Villamor Lopez <pedro88vl@gmail.com>
2018-11-07 15:10:37 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bristol = Restaurant.create(name: "Epicure", category: "french", address: "75008 Paris")
+london = Restaurant.create(name: "Beijing", category: "chinese", address: "London")
+oliver = Restaurant.create(name: "Oliver", category: "italian", address: "London")
+nandos = Restaurant.create(name: "Nando's", category: "belgian", address: "London")
+sushi = Restaurant.create(name: "Sushi House", category: "japanese", address: "London")
